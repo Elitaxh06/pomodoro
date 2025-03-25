2613fe7aeef1b851d71b0321aa6084187fcb0ccb
Elitaxh <vivaheredia2535@outlook.com>
2025-03-25 12:15:39 -0600

    Reset button now resets the counter based on the 'timer' state

--- src/Pages/Home/Timer/Timer.tsx
+++ src/Pages/Home/Timer/Timer.tsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react"
 function Timer() {
-    const [ time, setTime ] = useState(25 * 60)
-    const [ running, setRunning ] = useState(false)
-    const plus25 = () => setTime(time + 25 * 60)
-    const plus10 = () => setTime(time + 10 * 60)
-    const plus5 = () => setTime(time + 5 * 60)
-    const plus1 = () => setTime(time + 1 * 60)
-    const reset = () => setTime(25 * 60)
-    const modTime = () => setRunning(!running)
+    const [sessionType, setSessionType] = useState<"focus" | "shortBreak" | "longBreak">("focus")
+    const [ time, setTime ] = useState<number>(25 * 60)
+    const [ running, setRunning ] = useState<boolean>(false)
+    const plus25 = (): void => setTime((prev) => prev + 25 * 60)
+    const plus10 = (): void => setTime((prev) => prev + 10 * 60)
+    const plus5 =  (): void => setTime((prev) => prev + 5 * 60)
+    const plus1 =  (): void => setTime((prev) => prev + 1 * 60)
+    const resetTimer = () => {
+        switch(sessionType){
+            case "focus":
+                setTime(25 * 60);
+                break;
+            case "shortBreak":
+                setTime(5 * 60);
+                break;
+            case "longBreak":
+                setTime(15 * 60);
+                break;
+        }
+    }
+    const modTime =(): void => setRunning((prev) => !prev)
     useEffect(()=> {
         let interval: ReturnType<typeof setInterval> | undefined
         if (running) {
@@ -21,18 +34,25 @@ function Timer() {
     }, [running])
 
     return(
-        <section className="mt-20"> 
-            
-            <h1 className="text-5xl font-bold">{Math.floor(time / 60)}:{String(time % 60).padStart(2, "0")}</h1>
+        <section className="mt-20 border-l-2 border-slate-500 flex items-center justify-center flex-col"> 
+            <div className="mb-5 flex gap-4 text-slate-300">
+                <button className="cursor-pointer w-22 h-8 rounded-lg hover:bg-slate-700" onClick={() => {setSessionType("focus"); setTime(25 * 60)}}>Focus</button>
+                <button className="cursor-pointer w-26 h-8 rounded-lg hover:bg-slate-700" onClick={() => {setSessionType("shortBreak"); setTime(5 * 60)}}>Short Break</button>
+                <button className="cursor-pointer w-26 h-8 rounded-lg hover:bg-slate-700" onClick={()=> {setSessionType("longBreak"); setTime(15 * 60)}}>Long Break</button>
+                <button></button>
+            </div>
+            <h1 className="text-9xl font-bold">{Math.floor(time / 60)}:{String(time % 60).padStart(2, "00")}</h1>
             <div className="mt-5 flex gap-4 mb-4">
-                <button className="border border-slate-300 w-22" onClick={plus25}>+ 25 min</button>
-                <button className="border border-slate-300 w-22" onClick={plus10}>+ 10 min</button>
-                <button className="border border-slate-300 w-22" onClick={plus5}>+ 5 min</button>
-                <button className="border border-slate-300 w-22" onClick={plus1}>+ 1 min</button>
+                <button className="cursor-pointer text-slate-400 hover:text-white w-22" onClick={plus25}>+ 25 min</button>
+                <button className="cursor-pointer text-slate-400 hover:text-white w-22" onClick={plus10}>+ 10 min</button>
+                <button className="cursor-pointer text-slate-400 hover:text-white w-22" onClick={plus5}>+ 5 min</button>
+                <button className="cursor-pointer text-slate-400 hover:text-white w-22" onClick={plus1}>+ 1 min</button>
             </div>
             <aside className="flex gap-4">
-                <button className="border border-slate-300 w-22" onClick={modTime}>{running ? 'Stop' : 'Start'}</button>
-                <button className="border border-slate-300 w-22" onClick={reset}>Reset</button>
+                <button className={`border border-slate-300 w-22 cursor-pointer hover:scale-105 transition-transform  ${!running ? 'bg-green-400' : 'bg-red-400'}`} onClick={modTime}>{running ? 'Stop' : 'Start'}</button>
+                {time !== 25 * 60 && (
+                    <button className="border border-slate-300 w-22 cursor-pointer hover:scale-105 transition-transform bg-gray-600" onClick={resetTimer}>Reset</button>
+                )}
             </aside>
         </section>
     )
--- src/Pages/Home/todo/Todo.tsx
+++ src/Pages/Home/todo/Todo.tsx
@@ -2,7 +2,7 @@
 function Todo() {
     return(
         <section className="mt-20">
-            <h1 className="color-red">Drop Your Todos Here</h1>
+            <h1 className="text-xl">Lista de tareas</h1>
         </section>
     )
 
